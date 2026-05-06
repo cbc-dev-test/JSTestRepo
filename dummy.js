fb382f6793024ac0af5ca744610c5261321abdf0
SindhaPerumal <sperumal@cloudbees.com>
2026-05-06 19:43:11 +0530

    Updated dummy.js for testing

--- dummy.js
+++ dummy.js
@@ -4,10 +4,6 @@ var dummy_info = {
   // Customer module configs
   "users": [
     {
-      "username": "admin",
-      "password": "admin"
-    },
-    {
       "username": "roberto",
       "password": "asdfpiuw981"
     }
